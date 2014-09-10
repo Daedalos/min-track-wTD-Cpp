4fc1c007791d51e4291b8c47759e088afe9b4c3a
UMo <uimorone@gmail.com>
2014-09-10 23:15:13 +0800

    minor change makeplotsc

--- makeplotscript.py
+++ makeplotscript.py
@@ -4,16 +4,23 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-ntd = 0
-D = 11
+ntd = 1
+D = 6
 M = 1
-PATH=19
+PATH=50
 B=25
 minima = np.zeros((PATH*B,2))
+bad =0
 for p in range(PATH):
     data = np.loadtxt('path/D%d_M%d_PATH%d_Ntd%d.dat'%(D,M,p,ntd))
-    minima[B*p:B*(p+1),:] = data[:,[0,2]]
-    print "Failed Paths = ", sum(data[:,1]!=1)
+    if data.size==0:
+        print "EMPTRY!"
+        bad += 1
+    else:
+        minima[B*p:B*(p+1),:] = data[:,[0,2]]
+        print "Failed Paths = ", sum(data[:,1]!=1)
+
+
 np.savetxt('beta_minima.dat', minima, fmt='%e')
 
 #matplotlib.use("Agg")
@@ -22,11 +29,12 @@ matplotlib.rc('font', **font)
 minima = np.loadtxt('beta_minima.dat')
 fig = plt.figure(figsize=(12,9))
 ax1 = fig.add_subplot(111)
-ax1.scatter(minima[:,0],minima[:,1])
 ax1.set_title('Lorenz96 SingleF Dim={0} Meas={1} NTau={2}'.format(D,M,ntd))
 ax1.set_ylabel('Minimized Action')
 ax1.set_xlabel('beta')
 ax1.set_yscale('log')
+
+ax1.scatter(minima[:,0],minima[:,1])
 plt.savefig('Action_level_D{}_M{}_PATH{}_Ntd{}.png'.format(D,M,p,ntd),bbox_inches='tight')
 
 plt.show()
