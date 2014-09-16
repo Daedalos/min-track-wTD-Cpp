4d6cdefc6166a73b4e21d7db2692eccb8f5837aa
UMo <uimorone@gmail.com>
2014-09-16 08:16:34 +0800

    Use same init paths, data files

---
+++ matlab_generate_data/generate_data.py
@@ -0,0 +1,29 @@
+from numpy import *
+from scipy.integrate import odeint
+
+
+def lorenz96(x, t):
+    N = len(x)
+    dxdt = zeros(N)
+    for i in range(N):
+        dxdt[i] = x[mod(i-1,N)]*(x[mod(i+1,N)]-x[mod(i-2,N)]) - x[i] + 8.17
+
+    return dxdt
+
+dt = 0.01
+
+T_total = arange(0,100,dt)
+#data_initial = randn(5+1,1)
+data_initial = [0.80, 0.95, 0.71, 0.24, 0.63];
+#data_initial[-1] = 8.17
+
+Y = odeint(lorenz96,data_initial,T_total)
+Y = Y[1000:2501,:]
+param = 8.17*ones(len(Y))
+Y = column_stack((Y,param))
+
+savetxt("data_dt01.txt",Y)
+noise = 0.5*random.standard_normal(Y.shape)
+Ynoise = Y + noise
+savetxt("dataN_dt01.txt",Ynoise)
+
