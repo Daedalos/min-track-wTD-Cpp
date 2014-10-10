Fixed bad indexing with taus TD calculation

makeplotscript.py
@@ -4,17 +4,27 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-ntd = 2
+ntd = 0
+taus = []
 D = 5
 M = 1
-PATH=17
+PATH=100
 dt=0.01
 B=30
 minima = np.zeros((PATH*B,2))
 bad =0
+filetemp = 'path/D%d_M%d_PATH%d_Ntd%d_'
+for i in range(len(taus)):
+    filetemp += '%d-' % taus[i]
+filetemp += 'dt%e.dat'
+print filetemp
 for p in range(PATH):
 #    data = np.loadtxt('path/D%d_M%d_PATH%d_Ntd%d_dt%d.dat'%(D,M,p,ntd,dt))
-    data = np.loadtxt('path/D%d_M%d_PATH%d_Ntd%d_dt%e.dat'%(D,M,p,ntd,dt))
+    try:
+        
+        data = np.loadtxt(filetemp%(D,M,p,ntd,dt))
+    except: continue
+
     if data.size==0:
         print "EMPTRY!"
         bad += 1

numminima.py
@@ -1,28 +1,40 @@
-# Collect all the scripts to plot Action-levels in one file --URI
+# Count number of paths with Action below threshold
 
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
 ntd = 0
+taus = []
 D = 5
 M = 1
-PATH=50
+PATH=100
 dt=0.01
+Bstart = 20
 B=30
-thesh = 500
-action = np.zeros((B,PATH))
+thesh = 0.1
+action = np.zeros((B-Bstart,PATH))
 bad =0
+
+filetemp = 'path/D%d_M%d_PATH%d_Ntd%d_'
+for i in range(len(taus)):
+    filetemp += '%d-' % taus[i]
+filetemp += 'dt%e.dat'
+
 for p in range(PATH):
 #    data = np.loadtxt('path/D%d_M%d_PATH%d_Ntd%d_dt%d.dat'%(D,M,p,ntd,dt))
-    data = np.loadtxt('path/D%d_M%d_PATH%d_Ntd%d_dt%e.dat'%(D,M,p,ntd,dt))
+    try:
+        data = np.loadtxt(filetemp%(D,M,p,ntd,dt))
+    except: continue
+
     if data.size==0:
         print "EMPTRY!"
         bad += 1
     else:
+        data = data[Bstart:B,:]
         action[:,p] = data[:,2]
         print "Incomplete Paths = ", sum(data[:,1]!=1)
 
 numbelow = np.sum(action<thesh,1)
-plt.plot(numbelow)
+plt.plot(range(B-Bstart), numbelow)
 plt.show()

plotpaths.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
-ntd = 1
+ntd = 0
 D = 5
 M = 1
 PATH=100
-dt=0.01
-thresh = 13000
-lowerbnd = 11500
+dt= 0.1
+thresh = 11000
+lowerbnd = 0
 B=29
 N = 300
 
@@ -17,9 +17,17 @@ N = 300
 bad = 0
 Ydata = np.loadtxt('dataN_dt01_noP.txt')
 
+filetemp = 'path/D%d_M%d_PATH%d_Ntd%d_'
+for i in range(len(taus)):
+    filetemp += '%d-' % taus[i]
+filetemp += 'dt%e.dat'
+
 for p in range(PATH):
 #    data = np.loadtxt('path/D%d_M%d_PATH%d_Ntd%d_dt%d.dat'%(D,M,p,ntd,dt))
-    data = np.loadtxt('path/D%d_M%d_PATH%d_Ntd%d_dt%e.dat'%(D,M,p,ntd,dt))
+    try:
+        data = np.loadtxt(filetemp%(D,M,p,ntd,dt))
+    except: continue
+
     if data.size==0:
         print "EMPTRY!"
         bad += 1